9e0002d2b40c151035ec1b08ebe53c6cda4e2bbf
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-21 22:29:40 +0300

    new edits

--- App.js
+++ App.js
@@ -7,15 +7,14 @@ import Popular from "./scr/components/Popular"
 
 export default function App() {
   return (
-    <ScrollView style={{backgroundColor: COLORS.black,}}>
+    <ScrollView style={{ backgroundColor: COLORS.black }}>
       <View
         style={{
-          width: '100%',
-          height: '100%',
+          width: "100%",
+          height: "100%",
           padding: 10,
           paddingTop: 70,
           paddingBottom: 75,
-          // backgroundColor: COLORS.black,
         }}
       >
         <Header />
